3b45533b23f56a196f7c33af3f8387c8a7d6814a
n4beel <nabeel3353045805@hotmail.com>
2022-05-30 16:00:18 +0500

    Navigation Complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-const App = () => {
+import * as React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Assets from "./pages/Assets";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+
+function App() {
   return (
-    <div className="App">
-      <p>Hello World</p>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="assets" element={<Assets />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Routes>
+    </>
   );
-};
+}
 
 export default App;
